dfc9339b2e1d3685222d5b26cc5f05d7d87f1f44
tongwen <602943077@qq.com>
2017-07-26 22:39:06 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 use MbString\MbString;
-$a = new MbString;
-var_dump($a);
+$mb = new MbString;
+$str = '我是编码函数';
+$strRe = $mb->strRev($str); // 数函码编理处是我
